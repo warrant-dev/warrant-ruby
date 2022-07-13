4660971ca1d52df6954bc0a7036a06d52f603b25
stanleyphu <phu.stanley@gmail.com>
2022-07-13 14:35:17 -0700

    Add permission role assignment methods

--- lib/warrant/models/permission.rb
+++ lib/warrant/models/permission.rb
@@ -110,5 +110,54 @@ module Warrant
                 APIOperations.raise_error(res)
             end  
         end
+
+        # Assign a permission to a role
+        #
+        # @param permission_id [String] The permission_id of the permission you want to assign to a role.
+        # @param role_id [String] The role_id of the role you want to assign a permission to.
+        #
+        # @return [Permission] assigned permission
+        #
+        # @raise [Warrant::InternalError]
+        # @raise [Warrant::InvalidRequestError]
+        # @raise [Warrant::MissingRequiredParameterError]
+        # @raise [Warrant::NotFoundError]
+        # @raise [Warrant::UnauthorizedError]
+        # @raise [Warrant::WarrantError]
+        def self.assign_to_role(permission_id, role_id)
+            res = APIOperations.post(URI.parse("#{::Warrant.config.api_base}/v1/roles/#{role_id}/permissions/#{permission_id}"))
+
+            case res
+            when Net::HTTPSuccess
+                permission = JSON.parse(res.body)
+                Permission.new(permission['permissionId'])
+            else
+                APIOperations.raise_error(res)
+            end  
+        end
+
+        # Remove a permission from a role
+        #
+        # @param permission_id [String] The permission_id of the permission you want to assign to a role.
+        # @param role_id [String] The role_id of the role you want to assign a permission to.
+        #
+        # @return [nil] if remove was successful
+        #
+        # @raise [Warrant::InternalError]
+        # @raise [Warrant::InvalidRequestError]
+        # @raise [Warrant::MissingRequiredParameterError]
+        # @raise [Warrant::NotFoundError]
+        # @raise [Warrant::UnauthorizedError]
+        # @raise [Warrant::WarrantError]
+        def self.remove_from_role(permission_id, role_id)
+            res = APIOperations.delete(URI.parse("#{::Warrant.config.api_base}/v1/roles/#{role_id}/permissions/#{permission_id}"))
+
+            case res
+            when Net::HTTPSuccess
+                return
+            else
+                APIOperations.raise_error(res)
+            end  
+        end
     end
 end
