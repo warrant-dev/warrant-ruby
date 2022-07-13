8fb246d7f9affa73f7f030559f06cebfeaf9b7d6
stanleyphu <phu.stanley@gmail.com>
2022-07-12 21:43:52 -0700

    Update session class to have separate session methods

--- lib/warrant/models/session.rb
+++ lib/warrant/models/session.rb
@@ -2,19 +2,40 @@
 
 module Warrant
     class Session
-        attr_reader :user_id, :token
-
-        # @!visibility private
-        def initialize(user_id, token)
-            @user_id = user_id
-            @token = token
-        end
-
-        # Create an Authorization or Self-Service Dashboard session for a given user
+        # Create an Authorization Session for a given user
         #
-        # @option params [String] :type Type of session to be created. (Either "sess" or "ssdash")
         # @option params [String] :user_id Id of the user to create a session for.
         # @option params [Integer] :ttl Number of seconds a session should live for. By default session tokens live for 24 hours and self service tokens live for 30 minutes.
+        # 
+        # @return [String] Session token
+        #
+        # @raise [Warrant::InternalError]
+        # @raise [Warrant::InvalidParameterError]
+        # @raise [Warrant::InvalidRequestError]
+        # @raise [Warrant::MissingRequiredParameterError]
+        # @raise [Warrant::NotFoundError]
+        # @raise [Warrant::UnauthorizedError]
+        # @raise [Warrant::WarrantError]
+        def self.create_authorization_session(params = {})
+            params = params.merge(type: "sess")
+            res = APIOperations.post(URI.parse("#{::Warrant.config.api_base}/v1/sessions"), Util.normalize_params(params))
+
+            case res
+            when Net::HTTPSuccess
+                res_json = JSON.parse(res.body)
+                res_json['token']
+            else
+                APIOperations.raise_error(res)
+            end
+        end
+
+        # Create a Self-Service Dashboard Session for a given user
+        #
+        # @param redirect_url [String] URL to redirect to once self-service session is created
+        # @option params [String] :user_id Id of the user to create a session for.
+        # @option params [Integer] :ttl Number of seconds a session should live for. By default session tokens live for 24 hours and self service tokens live for 30 minutes.
+        #
+        # @return [String] URL to the self service dashboard 
         # 
         # @raise [Warrant::InternalError]
         # @raise [Warrant::InvalidParameterError]
@@ -23,13 +44,14 @@ module Warrant
         # @raise [Warrant::NotFoundError]
         # @raise [Warrant::UnauthorizedError]
         # @raise [Warrant::WarrantError]
-        def self.create(params = {})
+        def self.create_self_service_session(redirect_url, params = {})
+            params = params.merge(type: "ssdash")
             res = APIOperations.post(URI.parse("#{::Warrant.config.api_base}/v1/sessions"), Util.normalize_params(params))
 
             case res
             when Net::HTTPSuccess
                 res_json = JSON.parse(res.body)
-                Session.new(res_json['userId'], res_json['token'])
+                "#{::Warrant.config.self_service_dash_url_base}/#{res_json['token']}?redirectUrl=#{redirect_url}"
             else
                 APIOperations.raise_error(res)
             end
--- lib/warrant/warrant_configuration.rb
+++ lib/warrant/warrant_configuration.rb
@@ -5,10 +5,11 @@ module Warrant
     class WarrantConfiguration
         attr_accessor :api_key
 
-        attr_reader :api_base
+        attr_reader :api_base, :self_service_dash_url_base
 
         def initialize
             @api_base = "https://api.warrant.dev"
+            @self_service_dash_url_base = "https://self-serve.warrant.dev"
         end
     end
 end
